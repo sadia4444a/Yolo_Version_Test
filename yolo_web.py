ba1d376d83c5d05a2a012c1b204d71dd4211c8dc
Mst. Sadia khatun <sadiasultana4444a@gmail.com>
2024-10-11 16:28:22 +0600

    Add yolo version test  code

---
+++ yolo_web.py
@@ -0,0 +1,58 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+import time
+ 
+cap = cv2.VideoCapture(0)  # For Webcam
+# cap.set(3, 1680)
+# cap.set(4, 1050)
+
+# cap = cv2.VideoCapture("../Videos/motorbikes.mp4")  # For Video
+cv2.namedWindow("Image", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+ 
+model = YOLO("yolov8m.pt") # Change model weight here
+ 
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+              "teddy bear", "hair drier", "toothbrush"
+              ]
+ 
+prev_frame_time = 0
+new_frame_time = 0
+ 
+while True:
+    new_frame_time = time.time()
+    success, img = cap.read()
+    results = model(img, stream=True, device="mps")
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding Box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img,(x1,y1),(x2,y2),(255,0,255),3)
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+            # Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+            # Class Name
+            cls = int(box.cls[0])
+ 
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+ 
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    print(fps)
+ 
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
